c7aebdf49b089be1aa0d8b70711a19df187890db
Yashwant Chauhan <yashwantc90@gmail.com>
2014-03-09 21:37:22 +0100

    Update auto_hg.sh

--- auto_hg.sh
+++ auto_hg.sh
@@ -2,6 +2,8 @@
 # Copyright (c) 2014, Yashwant Chauhan
 # install.sh
 
+# Work in progress...
+
 hg init;
 hg add .;
 hg commit -m "automated commit message"; # TODO: make the commit message more descriptive of the changes. 

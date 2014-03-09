fc4bfd066c4eee8eaff362b16b76bc6404a30c9c
Yashwant Chauhan <yashwantc90@gmail.com>
2014-03-09 21:13:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,16 +9,13 @@ cd ~/.quicksilver;
 touch watchlist.txt;
 echo "~/.quicksilver/testFolder\n" > watchlist.txt;
 ruby gen_plist.rb;
-if [ -f "com.yashwantc.quicksilver.plist" ]
-then
+if [ -f "com.yashwantc.quicksilver.plist" ]; then
 	ln -s ~/.quicksilver/com.yashwantc.quicksilver.plist ~/Library/LaunchAgents
 	launchctl load ~/Library/LaunchAgents/com.yashwantc.quicksilver.plist;
 	echo "quicksilver is installed! You can find it at ~/.quicksilver";
 
-	if [ -f "~/.quicksilver/testFile.txt" ]	
-	then
+	if [ -f "~/.quicksilver/testFile.txt" ]; then
 		echo "Tip: Currently quicksilver only watches ~/.quicksilver/testFile.txt to make it watch another file edit the path name in com.yashwantc.quicksilver.plist and delete testFile.txt";
-	else 
 	fi
 else 
 	echo "Error: Couldn't find com.yashwantc.quicksilver.plist! Failed installation!";

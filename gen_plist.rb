af6fb1f2084d1040c5edef7e44b1221ae281d5dd
Yashwant Chauhan <yashwantc90@gmail.com>
2014-03-09 21:30:17 +0100

    Update gen_plist.rb

--- gen_plist.rb
+++ gen_plist.rb
@@ -11,7 +11,7 @@ unless watchlist.empty?
 		plist.to_plist
 		File.open("com.yashwantc.quicksilver.plist", 'w+') {|f| f.write(plist.to_plist) }
 		unless plist.nil? && File.exist?("com.yashwantc.quicksilver.plist")
-			puts "Generated com.yashwantc.quicksilver.plist!"
+			puts "Baked cake..."
 		end
 	end
 end

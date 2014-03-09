8021bd72460bda051fcc04f2f11609075f50c385
Yashwant Chauhan <yashwantc90@gmail.com>
2014-03-09 21:29:34 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,11 +8,7 @@ mv quicksilver ~/.quicksilver;
 cd ~/.quicksilver;
 touch watchlist.txt;
 echo "~/.quicksilver/testFolder\n" > watchlist.txt;
-echo "Wrote default watchlist.txt...";
 ruby gen_plist.rb;
-echo "Wrote LaunchAgent plist...";
 ln -s ~/.quicksilver/com.yashwantc.quicksilver.plist ~/Library/LaunchAgents
-echo "Created a symlink for plist...";
 launchctl load ~/Library/LaunchAgents/com.yashwantc.quicksilver.plist;
-echo "Quicksilver set as LaunchAgent...";
 echo "Quicksilver is installed! You can find it at ~/.quicksilver";

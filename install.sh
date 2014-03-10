896edad4abf585359aa97ac38d700a9ebeb8a1ca
Yashwant Chauhan <yashwantc90@gmail.com>
2014-03-10 21:04:30 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ cd ~/.quicksilver;
 echo "Writing invite list...";
 touch watchlist.txt; 
 printf "~/.quicksilver/testFolder\n" > watchlist.txt; 
-ruby gen_plist.rb
+ruby ~/.quicksilver/qs/rb/gen_plist.rb
 ln -s ~/.quicksilver/com.yashwantc.quicksilver.plist ~/Library/LaunchAgents
 echo "Greeting guests...";
 launchctl load ~/Library/LaunchAgents/com.yashwantc.quicksilver.plist;

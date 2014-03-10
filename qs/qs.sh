29a219689bf27ec94ee0e7fcbe9ea5af764d65df
Yashwant Chauhan <yashwantc90@gmail.com>
2014-03-10 21:22:06 +0100

    Update qs.sh

--- qs/qs.sh
+++ qs/qs.sh
@@ -2,21 +2,21 @@
 # Copyright (c) 2014, Yashwant Chauhan
 # qs.sh
 
-if [ -z $1 ]; then
-  if [ $1 == "-l" || $1 == "--launch" ]; then
+if [ ! -z $1 ]; then
+  if [ $1 == "-l" -o $1 == "--launch" ]; then
     ruby ~/.quicksilver/qs/rb/launch.rb
   fi
-  if [ $1 == "-rw" || $1 == "--reload_watchlist" ]; then
+  if [ $1 == "-rw" -o $1 == "--reload_watchlist" ]; then
     unlink ~/Library/LaunchAgents/com.yashwantc.quicksilver.plist
     rm -rf ~/.quicksilver/com.yashwantc.quicksilver.plist
     ruby ~/.quicksilver/qs/rb/gen_plist.rb
     ln -s ~/.quicksilver/com.yashwantc.quicksilver.plist ~/Library/LaunchAgents
   fi
-  if [ $1 == "-la" || $1 == "--launch_agent" ]; then
+  if [ $1 == "-la" -o $1 == "--launch_agent" ]; then
     echo "The agent is located at: ~/Library/LaunchAgents/com.yashwantc.quicksilver.plist"
     # open ~/Library/LaunchAgents/com.yashwantc.quicksilver.plist
   fi
-  if [ $1 == "-p" || $1 == "--path" ]; then
+  if [ $1 == "-p" -o $1 == "--path" ]; then
     echo "The Quicksilver directory is located at: ~/.quicksilver\n"
     # open ~/.quicksilver
   fi

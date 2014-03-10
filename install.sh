507bdc6116424129d966250b27c8d5c33e9f8753
Yashwant Chauhan <yashwantc90@gmail.com>
2014-03-10 21:32:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,5 @@ ruby ~/.quicksilver/qs/rb/gen_plist.rb
 ln -s ~/.quicksilver/com.yashwantc.quicksilver.plist ~/Library/LaunchAgents
 mv ~/.quicksilver/qs/qs.sh ~/.quicksilver/qs/qs
 chmod 755 ~/.quicksilver/qs/qs
-ln -s ~/.quicksilver/qs/qs /usr/bin
 launchctl load ~/Library/LaunchAgents/com.yashwantc.quicksilver.plist
 echo "Quicksilver installed :)"

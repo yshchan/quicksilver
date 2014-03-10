86229831f185e856281d9791402e51cc7b5a672a
Yashwant Chauhan <yashwantc90@gmail.com>
2014-03-10 19:02:48 +0100

    Revert 8ed14c5..7bbf4b4
    This rolls back to commit 8ed14c50fa487f7ae99e4d5997a84c56b12d6b5f.

--- qs/gen_plist.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2014, Yashwant Chauhan
-# gen_plist.sh
-
-ruby ~/.quicksilver/qs/gen_plist.rb
--- qs/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2014, Yashwant Chauhan
-# run.sh
-
-ruby ~/.quicksilver/qs/qs.rb

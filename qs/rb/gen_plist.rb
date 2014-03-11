d7b7c20073761984bc9f40cfd43d300fb53d12ab
Yashwant Chauhan <yashwantc90@gmail.com>
2014-03-11 12:18:40 +0100

    Update gen_plist.rb
    Added StartInterval

--- qs/rb/gen_plist.rb
+++ qs/rb/gen_plist.rb
@@ -8,7 +8,7 @@ watchlist = File.open("#{Dir.home}/.quicksilver/watchlist.txt","rb").read
 unless watchlist.empty?
 	list = watchlist.split("\n")
 	if list.any?
-		plist = {"Label"=>"com.yashwantc.quicksilver.plist", "OnDemand"=>true, "EnableGlobbing"=>true, "KeepAlive"=>true, "ProgramArguments"=>["bash","~/.quicksilver/qs/qs.sh","-l"],"WatchPaths"=>list}
+		plist = {"Label"=>"com.yashwantc.quicksilver", "OnDemand"=>true, "StartInterval"=>60, "EnableGlobbing"=>true, "KeepAlive"=>true, "ProgramArguments"=>["~/.quicksilver/qs/qs","-l"],"WatchPaths"=>list}
 		plist.to_plist
 		File.open("com.yashwantc.quicksilver.plist", 'w+') {|f| f.write(plist.to_plist) }
 	end
